e120a868051f406ba18d774ab3c70eedca379e35
Lamiaa21-3 <lamiaagamal213@gmail.com>
2025-05-11 11:27:39 +0200

    fix buge in login one

--- lib/features/login_one/login_one_screen.dart
+++ lib/features/login_one/login_one_screen.dart
@@ -1,9 +1,11 @@
 import 'package:ahdydic/core/helper/extensions.dart';
 import 'package:ahdydic/core/helper/spacing.dart';
-import 'package:ahdydic/core/theming/color.dart';
+import 'package:ahdydic/core/routing/routes.dart';
+
 import 'package:ahdydic/core/theming/images.dart';
 import 'package:ahdydic/core/theming/styles.dart';
-import 'package:ahdydic/core/widgets/custom_button.dart';
+
+import 'package:ahdydic/features/login_one/widgets/login_one_row_divider.dart';
 import 'package:ahdydic/features/onboarding/onboarding_one/widgets/onboarding_one_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,13 +20,15 @@ class LoginOneScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-
           children: [
-
             Padding(
-              padding:  EdgeInsets.only(right: 300.0.w
+              padding: EdgeInsets.only(right: 300.0.w),
+              child: IconButton(
+                onPressed: () {
+                  context.pop();
+                },
+                icon: Icon(Icons.arrow_forward_ios),
               ),
-              child: IconButton(onPressed: (){context.pop();}, icon: Icon(Icons.arrow_forward_ios,)),
             ),
             Image.asset(ImagesManager.loginOneBackground),
             verticalSpace(110),
@@ -32,41 +36,21 @@ class LoginOneScreen extends StatelessWidget {
               S.of(context).welcomBack,
               style: StylesManager.font24DartPurpleBold,
             ),
-            OnboardingOneButton(
-
-            ),
+            OnboardingOneButton(),
             verticalSpace(12),
             LoginOneRowDivider(),
-            Text(S.of(context).completeUsingAnotherMethod,style: StylesManager.font18PinkMedium,)
-
+            InkWell(
+              onTap: (){
+                context.pushNamed(Routes.loginScreen);
+              },
+              child: Text(
+                S.of(context).completeUsingAnotherMethod,
+                style: StylesManager.font18PinkMedium,
+              ),
+            ),
           ],
         ),
       ),
-    );
-  }
-}
-class LoginOneRowDivider extends StatelessWidget {
-  const LoginOneRowDivider({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return   Row(
-      children: [
-        horizontalSpace(20),
-        Container(
-          width: 130.w,
-          height: 1.h,
-          color: ColorManager.lightGrey,
-        ),
-        horizontalSpace(23),
-        Text('او',style: StylesManager.font20GreyMedium,),
-        horizontalSpace(23),
-        Container(
-          width: 130.w,
-          height: 1.h,
-          color: ColorManager.lightGrey,
-        ),
-      ],
     );
   }
 }
---
+++ lib/features/login_one/widgets/login_one_row_divider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/helper/spacing.dart';
+import '../../../core/theming/color.dart';
+import '../../../core/theming/styles.dart';
+
+class LoginOneRowDivider extends StatelessWidget {
+  const LoginOneRowDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   Row(
+      children: [
+        horizontalSpace(20),
+        Container(
+          width: 130.w,
+          height: 1.h,
+          color: ColorManager.lightGrey,
+        ),
+        horizontalSpace(23),
+        Text('او',style: StylesManager.font20GreyMedium,),
+        horizontalSpace(23),
+        Container(
+          width: 130.w,
+          height: 1.h,
+          color: ColorManager.lightGrey,
+        ),
+      ],
+    );
+  }
+}
