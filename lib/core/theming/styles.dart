9628eb3f9cdf978e3bb6791d848e428d48df6f64
Lamiaa21-3 <lamiaagamal213@gmail.com>
2025-06-11 16:20:30 +0200

    add fonts

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -11,11 +11,13 @@ class StylesManager {
     fontSize: 30.sp,
     color: ColorManager.teal,
     fontWeight: FontWeightHelper.bold,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font16MorePurpleRegular = TextStyle(
     fontSize: 16.sp,
     color: ColorManager.morePurple,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font14DarkPurpleBold = TextStyle(
     fontSize: 14.sp,
@@ -26,28 +28,33 @@ class StylesManager {
     fontSize: 18.sp,
     color: ColorManager.white,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font14PinkRegular = TextStyle(
     fontSize: 14.sp,
     color: ColorManager.pink,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font12DarkGrayRegular = TextStyle(
     fontSize: 12.sp,
     color: ColorManager.darkGrey,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
 
   static TextStyle font12WhiteRegular = TextStyle(
     fontSize: 12.sp,
     color: ColorManager.white,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
 
   static TextStyle font16PurplestLight = TextStyle(
     fontSize: 16.sp,
     color: Color(0xff533B5D),
     fontWeight: FontWeightHelper.light,
+    fontFamily: 'Tajawal',
   );
 
 
@@ -55,6 +62,7 @@ class StylesManager {
     fontSize: 14.sp,
     color: ColorManager.morePurple,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
 
 ////////////////////////////
@@ -62,6 +70,7 @@ class StylesManager {
     fontSize: 18.sp,
     color: ColorManager.boldBlue,
     fontWeight: FontWeightHelper.bold,
+    fontFamily: 'Tajawal',
   );
 
 
@@ -70,49 +79,58 @@ class StylesManager {
     fontSize: 18.sp,
     color: ColorManager.boldBlue,
     fontWeight: FontWeightHelper.bold,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font14DartBlueMedium = TextStyle(
     fontSize: 14.sp,
     color: ColorManager.teal,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
 
   static TextStyle font22DartPurpleRegular = TextStyle(
     fontSize: 22.sp,
     color: ColorManager.morePurple,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font24DartPurpleBold = TextStyle(
     fontSize: 24.sp,
     color: ColorManager.purple,
     fontWeight: FontWeightHelper.bold,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font30White500 =  TextStyle(
     fontSize: 30.sp,
     color: Colors.white,
     fontWeight: FontWeight.w500,
+    fontFamily: 'Tajawal',
   );
 ///////////////////////////
   static TextStyle font23LightGrayRegular =  TextStyle(
     fontSize: 23.sp,
     color: ColorManager.grey,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
   ///////////////////////////////
   static TextStyle font30DarkGreenRegular =  TextStyle(
     fontSize: 30.sp,
     color: ColorManager.darkGreen,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font17LightGrayRegular = TextStyle(
     fontSize: 15.sp,
     color: ColorManager.grey,
     fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font24DarkPurpleRegular = TextStyle(
     fontSize: 24.sp,
     color: ColorManager.purple,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
 
 
@@ -120,21 +138,25 @@ class StylesManager {
     fontSize: 18.sp,
     color: Color(0xff393939),
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font42DarkPurpleRegular = TextStyle(
     fontSize: 42.sp,
     color: ColorManager.purple,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font20GreyMedium = TextStyle(
     fontSize: 20.sp,
     color: ColorManager.grey,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
   static TextStyle font18PinkMedium = TextStyle(
     fontSize: 18.sp,
     color: ColorManager.pink,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
 
 
@@ -142,6 +164,7 @@ class StylesManager {
     fontSize: 20.sp,
     color: ColorManager.purple,
     fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Tajawal',
   );
 
 
@@ -149,6 +172,7 @@ class StylesManager {
     fontSize: 14.sp,
     color: ColorManager.pink,
     fontWeight: FontWeightHelper.bold,
+    fontFamily: 'Tajawal',
   );
 
 }
